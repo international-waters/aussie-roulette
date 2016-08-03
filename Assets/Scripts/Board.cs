1b6de7c6cbcf8161a4c74ea44c8b6f4d8d42584f
jcottell <s3242784@student.rmit.edu.au>
2016-08-03 12:34:38 +0930

    replaced deleted files
    Had heaps of trouble merging.
    
    I have manually merged Aarons work into the game.
    
    I have also added a Spin in Different Scene button and some logic to get
    that to work.
    (Also a temporary spin scene and script)
    
    There are some bugs:
    Spin in new scene seems to work fine for a while and then stops.
    Chip stack counters are being left of the table some times.
    
    I don't have time to find them right now

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,203 @@
+﻿/****************************************************************************
+* Building IT Systems (CPT 111 / COSC 2635) SP2, 2016
+* Game: Aussie Roulette  Group: International Waters
+* Authors : Aaron Horton s3465420, David Morling s3492242
+* Jeremy Cottell s3242784, Scott Nelson s3363315 , Simon Overton s3397924
+*
+* 
+****************************************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Board : MonoBehaviour {
+
+	public List<GameObject> betSpaces;
+	public List<ChipInfo> savedChips;
+	public int SelectedChipValue = 1;
+	public bool isTakingBets;
+	private GameManager game;
+
+
+	public static Board instance { get; private set;}
+	void Awake () {
+		//game must be started from the start screen in order to create the peristant
+		//game manager object
+		if (GameObject.Find ("GameManager") == null){
+			Destroy (gameObject);
+			SceneManager.LoadScene ("StartScreen");
+		}
+		//singleton pattern for keeping alive accross scenes
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad (gameObject);
+		} else {
+			Destroy (gameObject);
+		}
+	}
+
+
+	void Start () {
+
+		//create the betting grid gameobjects and store them
+		betSpaces = gameObject.GetComponent<GridConstructor> ().CreateBettingSpaces ();
+		isTakingBets = true;
+	}
+		
+	void OnLevelWasLoaded(){
+		//Toggle table, bets etc active if gamescreen is loaded otherwise deactivate
+		bool isThisLevelLoaded = (SceneManager.GetActiveScene ().name == "GamePlayScreen") ? true : false;
+
+		//hide the roulette table
+		gameObject.GetComponent<SpriteRenderer>().enabled = isThisLevelLoaded;
+
+		//hide all the chips and stack counter labels etc.
+		foreach (Transform child in transform) {
+			if (child.name != "BetMarker(Clone)") {
+				child.gameObject.SetActive (isThisLevelLoaded);
+			}
+		}
+		if (isThisLevelLoaded) {
+			//check to see if this screen is returning from a wheel spin
+			//and process the winning number
+			game = GameObject.Find("GameManager").GetComponent<GameManager>();
+			if (game.winNumberFlag != -1 && game.winNumberFlag <= 36) {
+
+				Text winLbl = GameObject.Find ("winningNumber").GetComponent<Text> ();
+				winLbl.text = game.winNumberFlag.ToString ();
+
+				game.ProcessWinNumber (this);
+			}
+
+		}
+	}
+
+
+
+	//this method clears all the chips owned by the player from the table and credits
+	//the player for each chip removed
+	public void ClearAllBets(Player playerToCredit)
+	{
+		//iterate through the list of bet Spaces to find those that contain bets
+		foreach (GameObject betSpaceObj in betSpaces) {
+			BoardBetSpace betSpace = betSpaceObj.GetComponent<BoardBetSpace> ();
+			if (playerToCredit != null) {
+				betSpace.RemoveAllChips (playerToCredit);
+				playerToCredit.CurrentBetTotal = 0;
+			} else {
+				betSpace.RemoveAllChips ();
+			}
+		}
+	}
+
+	public void ClearAllBets(){
+		ClearAllBets (null);
+	}
+
+	public void ClearLosingBets(int winningNumber){
+		foreach (GameObject betSpaceObj in betSpaces)
+		{
+			BoardBetSpace betSpace = betSpaceObj.GetComponent<BoardBetSpace> ();
+			int[] winners = betSpace.winNumbers;
+			bool winningSpace = false;
+			foreach (int number in winners) {
+				if (number == winningNumber) {
+					winningSpace = true;
+					break;
+				}
+			}
+			if (!winningSpace) {
+				betSpace.RemoveAllChips ();
+			}
+		}
+	}
+
+	public int CalculatePlayersTotalBet(Player player){
+		int total = 0;
+		foreach (GameObject betSpaceObj in betSpaces) {
+			BoardBetSpace betSpace = betSpaceObj.GetComponent<BoardBetSpace> ();
+			total += betSpace.PlacedChipsTotalValue (player);
+		}
+		return total;
+	}
+
+	//used to check that player can afford to repeat all saved bets before placing any.
+	//the newChipValue field is used when changing all chips to a different value
+	//O means no change
+	private int CalcTotalSavedBetValue(Player player,int newChipValue = 0){
+		int total = 0;
+		if (savedChips != null){
+			foreach (ChipInfo chip in savedChips) {
+				if (chip.ownedByPlayer == player.playerName) {
+					if (newChipValue == 0) {
+						total += chip.value;
+					} else {
+						total += newChipValue;
+					}
+				}
+			}
+		}
+		return total;
+	}
+/// <summary>
+/// Calulates and pays any winnings to the player
+/// </summary>
+/// <returns>The amount won for information purposes</returns>
+/// <param name="winningNumber">Winning number.</param>
+/// <param name="player">Player.</param>
+	public void PayoutWinnings(int winningNumber, Player player){
+		int winnings = 0;
+		foreach (GameObject betSpaceObj in betSpaces)
+		{
+			BoardBetSpace betSpace = betSpaceObj.GetComponent<BoardBetSpace> ();
+			int[] winners = betSpace.winNumbers;
+			bool winningSpace = false;
+			foreach (int number in winners) {
+				if (number == winningNumber) {
+					winningSpace = true;
+					break;
+				}
+			}
+			if (winningSpace) {
+				winnings += betSpace.CalculateWinnings (player);
+			}
+		}
+		player.RecieveWinnings (winnings);;
+	}
+
+	public void PlaceAllStoredChips(Player CurrentPlayer, int newChipValue = 0){
+		//clear any bets from the table and credit player
+		ClearAllBets(CurrentPlayer);
+		//check that player can afford to place all of the bets
+		if (CurrentPlayer.Wallet >= this.CalcTotalSavedBetValue (CurrentPlayer, newChipValue)) {
+
+			foreach (ChipInfo chip in savedChips) {
+				BoardBetSpace betSpace = betSpaces [chip.betSpaceId]
+				.GetComponent<BoardBetSpace> ();
+				if (newChipValue == 0) {
+					betSpace.PlaceChip (CurrentPlayer, chip.value);
+				} else {
+					betSpace.PlaceChip (CurrentPlayer, newChipValue);
+				}
+			}
+		}
+	}
+
+	public void ClearStoredChipHistory(){
+		savedChips = new List<ChipInfo> ();
+	}
+
+	public void StoreAllPlacedChipInfo(){
+		savedChips = new List<ChipInfo> ();
+		foreach (GameObject betSpaceObj in betSpaces) {
+			BoardBetSpace betSpace = betSpaceObj.GetComponent<BoardBetSpace> ();
+			foreach (GameObject placedChipObj in betSpace.placedChips) {
+				Chip placedChip = placedChipObj.GetComponent<Chip> ();
+				savedChips.Add (placedChip.ToChipInfo ());
+			}
+		}
+	}
+
+}
---
+++ Assets/Scripts/GameScreenUIcontroller.cs
@@ -0,0 +1,106 @@
+﻿/****************************************************************************
+* Building IT Systems (CPT 111 / COSC 2635) SP2, 2016
+* Game: Aussie Roulette  Group: International Waters
+* Authors : Aaron Horton s3465420, David Morling s3492242
+* Jeremy Cottell s3242784, Scott Nelson s3363315 , Simon Overton s3397924
+*
+* 
+****************************************************************************/
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class GameScreenUIcontroller : MonoBehaviour {
+	private Board board;
+	private Dropdown winNumber;
+	private Text winnerlbl;
+	private Dropdown chipValueList;
+	private GameManager game;
+	private BetView betView;
+	private BetController betController;
+
+	void Start(){
+		game = GameObject.Find ("GameManager").GetComponent<GameManager> ();
+		board = GameObject.Find ("RouletteTable").GetComponent<Board> ();
+		winNumber = GameObject.Find("winNumberDropdown").GetComponent<Dropdown>();
+		winnerlbl = GameObject.Find("winningNumber").GetComponent<Text>();
+		chipValueList = GameObject.Find("ChipValueList").GetComponent<Dropdown>();
+		game.RefreshScorePanel ();
+	}
+		
+
+	public void OnBackButtonClick(){
+		SceneManager.LoadScene ("StartScreen");
+	}
+
+	public void OnSaveButtonClick(){
+		
+		game.SaveGame(game.player, board);
+		board.ClearAllBets ();
+		game.RefreshScorePanel ();
+	}
+
+	public void OnLoadButtonClick(){
+		game.player = game.LoadGame(game.player.playerName, board);
+		game.player.CurrentBetTotal = board.CalculatePlayersTotalBet (game.player);
+		game.RefreshScorePanel ();
+		board.ClearStoredChipHistory ();
+	}
+
+	public void OnClearBetsButtonCick(){
+		board.StoreAllPlacedChipInfo ();
+		board.ClearAllBets (game.player);
+		game.RefreshScorePanel ();
+	}
+
+	public void OnTestWinnerButtonClick(){
+		int winner = winNumber.value;
+		winnerlbl.text = winner.ToString ();
+		game.ProcessWinNumber (board,winner);
+	} 
+
+	public void OnSpinButtonClick(){
+		int winner = (int)Mathf.Round(Random.Range (0f, 36f));
+		winnerlbl.text = winner.ToString ();
+		game.ProcessWinNumber (board,winner);
+
+	} 
+
+	public void OnSpinDiffSceneButtonClick(){
+		SceneManager.LoadScene ("SpinTestScene");
+	}
+
+	public void OnLoadLastBetsButtonClick(){
+		board.ClearAllBets (game.player);
+		board.PlaceAllStoredChips(game.player,board.SelectedChipValue);
+		game.RefreshScorePanel ();
+	}
+
+	public void OnChipValueListChanged(){
+
+		int value = 1;
+		switch (chipValueList.value) {
+		case 0:
+			break;
+		case 1:
+			value = 5;
+			break;
+		case 2:
+			value = 10;
+			break;
+		case 3:
+			value = 25;
+			break;
+		
+		case 4:
+			value = 50;
+			break;
+		}
+		board.ClearAllBets (game.player);
+		board.SelectedChipValue = value;
+		game.RefreshScorePanel ();
+	}
+		
+}
